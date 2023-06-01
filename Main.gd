52215280a0d7fa87cfac2f21bc893c6e7eee0629
smschijf <210578@student.glu.nl>
2023-06-01 13:41:53 +0200

    connected HUD to main scene

--- Main.gd
+++ Main.gd
@@ -3,17 +3,19 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
+
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout():
@@ -44,7 +46,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
